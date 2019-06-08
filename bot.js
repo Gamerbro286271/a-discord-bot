f23ef20d36622aa1c1990c8ddd738718bdd5466d
Gamerbro286271 <49330564+Gamerbro286271@users.noreply.github.com>
2019-06-08 14:27:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const PREFIX = '!';
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -14,7 +16,7 @@ client.on('guildMemberAdd', member => {
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+    if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
