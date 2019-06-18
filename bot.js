5d684360011165c353ea8c14236012025c44c0d8
Gamerbro286271 <49330564+Gamerbro286271@users.noreply.github.com>
2019-06-18 03:46:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,9 +23,6 @@ client.on('message', message=>{
 		case 'ping':
 			message.channel.sendMessage('pong!')
 			break;
-		case 'say':
-			message.channel.sendEmbed.message
-			if(!message.member.roles.find(r => r.name === "Staff")) return message.channel.send('YOU DO NOT HAVE PERMISSIONS')
 		case 'clear':
 			if(!message.member.roles.find(r => r.name === "Staff")) return message.channel.send('YOU DO NOT HAVE PERMISSIONS')
 			if(!args[1]) return message.reply('Error please define a certain amount')
